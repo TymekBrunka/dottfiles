d3e02235e69d2987a961e5127c6bd32cd020b9f9
TymekBrunka <tymkopolo@wp.pl>
2024-02-02 23:10:55 +0100

    added extras

--- make-dotfiles.sh
+++ make-dotfiles.sh
@@ -30,3 +30,30 @@ function copylisted {
 copylisted ~/.config/ ~/dottfiles/config ~/dottfiles/config-dirs ".config/"
 copylisted /usr/share/themes/ ~/dottfiles/themes ~/dottfiles/themes-tcp "themes/"
 copylisted /usr/share/icons/ ~/dottfiles/icon-themes ~/dottfiles/icon-themes-tcp "icons/"
+
+rm -rf ~/dottfiles/extras/*
+
+extras=$(cat ~/dottfiles/extra)
+echo $extras
+# IFS=$'| ' read -r -a extras <<< "$extras"
+#converting to array
+SAVEIFS=$IFS 
+IFS=$'\t'
+extras=($extras)
+IFS=$SAVEIFS
+
+echo ${extras[@]}
+for i in ${extras[@]}; do
+	IFS=$';' read -r -a elements <<< "$i" # cutting linse into slicessss
+	if [[ ${elements[1]::1} == "~" ]]; then
+		elements[1]="/home/$(whoami)${elements[1]:1}" # this bs of ~/... isn't file or directory
+	fi
+
+	if [[ ${elements[2]: -1} == $'|' ]]; then
+		elements[2]="${elements[2]::(${#elements[2]}-1)}" # cutting off separator
+	fi
+	echo "name: ${elements[0]}, from: ${elements[1]}, to: ~/dottfiles/extras/${elements[2]}"
+	# echo "$(echo $i | awk '{print $2}') to $(echo $i | awk '{print $3}')"
+	mkdir -p "/home/$(whoami)/dottfiles/extras/${elements[2]}"
+	cp -r -p "${elements[1]}"* "/home/$(whoami)/dottfiles/extras/${elements[2]}"
+done
